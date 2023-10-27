516c61cd9fc7df046f79e0a690657d484f6f57a3
Rob <61951374+Leo4j@users.noreply.github.com>
2023-10-27 12:22:19 +0100

    Update Invoke-SessionHunter.ps1

--- Invoke-SessionHunter.ps1
+++ Invoke-SessionHunter.ps1
@@ -578,7 +578,7 @@ function Invoke-SessionHunter {
 	}
 	
  	# Show Results
-	$FinalResults = $allResults | Sort-Object -Unique Domain,Access,AdmCount,HostName,UserSession | Format-Table -AutoSize | Out-String
+	$FinalResults = $allResults | Sort-Object -Unique Domain,Access,AdmCount,HostName,UserSession | Format-Table -AutoSize -Wrap | Out-String -Width 4096
 	$lines = $FinalResults -split "`n"
 	foreach($line in $lines) {
 	    Write-Output $line
